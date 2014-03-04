cc2b439fec2101939ba6f688d3c529cc17c7a1da
leeight <leeight@gmail.com>
2014-03-04 16:20:43 +0800

    Update base/upload.sh

--- base/upload.sh
+++ base/upload.sh
@@ -11,6 +11,8 @@ API_REPOS_LOCATION="$3"
 
 cd "${API_REPOS_LOCATION}"
 git checkout gh-pages
+git reset --hard origin/HEAD
+git pull origin gh-pages
 
 mkdir -p $(dirname "$SERVER_DOC_DIR")
 [ -e "${SERVER_DOC_DIR}" ] && git rm -r "${SERVER_DOC_DIR}"
